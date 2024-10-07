f9157a3111da565329439cc556db4df8d4a2dd88
swstkswstk <cs21b2035@iiitdm.ac.in>
2024-10-07 15:04:50 +0530

    fixed

--- script.js
+++ script.js
@@ -7,6 +7,11 @@
       if (weight == "") {
         lblResult.innerHTML = `Enter Some value for conversion`;
       }
+      else if(weight<0){
+        lblResult.innerHTML = `Enter a sane value for conversion`;
+
+
+      }
       else {
         let convert = weight * 2.20462;
         lblResult.innerHTML = `Converted Weight is :${convert}lbs`;
