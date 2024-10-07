f86c711f2cb6234ba64accc7db6f4fc8e7c02521
swstkswstk <cs21b2035@iiitdm.ac.in>
2024-10-07 14:53:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.getElementById("calculate");
+    let lblResult = document.getElementById("lblResult");
+    function calculate() {
+      let weight = document.getElementById("weight").value;
+      if (weight == "") {
+        lblResult.innerHTML = `Enter Some value for conversion`;
+      }
+      else {
+        let convert = weight * 2.20462;
+        lblResult.innerHTML = `Converted Weight is :${convert}lbs`;
+      }
+
+    }
+    btn.addEventListener('click', calculate);
+  })
+
+})();
